6da7a4ba27b5eb6ec6d7179870a5ecdcdcada84c
joelsonRamos <joelson.engcomp@gmail.com>
2022-03-28 13:53:45 -0300

    testes

---
+++ negocios/files.test.js
@@ -0,0 +1,35 @@
+const files = require('./files')
+const expect = require('chai').expect
+const fs = require('fs')
+const sinon = require('sinon')
+
+describe('reading directory', () => {
+    afterEach(done =>{
+        fs.readdir.restore()
+        done()
+    })
+    it('reads a directory', () => {
+        sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+            cb(null, ['file1.txt'])
+        })
+        const path = './'
+        return files.readdir(path)
+        .then( list =>{
+            expect(list.length).to.equals(1)
+            expect(fs.readdir.getCall(0).args[0]).to.equals(path)
+
+        })
+    } )
+    it('fails reading a directory', () => {
+        sinon.stub(fs, 'readdir').callsFake((path, cb) => {
+            cb('error')
+        })
+        const path = './'
+        return files.readdir(path)
+        .catch( err =>{
+            expect(err).to.equals('error')
+            expect(fs.readdir.getCall(0).args[0]).to.equals(path)
+
+        })
+    } )
+})
